98790445e5b4d0422409c087a07fe82a9aa8a792
jojmaDigital <servicejojma@gmail.com>
2025-09-25 12:23:25 +0200

    traduire all page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,12 @@ import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://kingludo.com",   // ton domaine public doit être correct et en https
+  site: "https://kingludo.com",
   output: "static",
   adapter: cloudflare(),
 
   integrations: [
     tailwind(),
-    sitemap({
-      filter: (page) => !page.pathname.includes("404"), // exclure 404 du sitemap
-      i18n: {
-        defaultLocale: "fr",        // langue principale de ton site
-        locales: {
-          fr: "fr",
-          en: "en",
-        },
-      },
-    }),
+    sitemap(),
   ],
 });
