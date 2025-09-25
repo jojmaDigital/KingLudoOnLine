2e650a152cddb31733c935c185f2ce3f8135c912
jojmaDigital <servicejojma@gmail.com>
2025-09-25 11:54:27 +0200

    traduire all page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://freecellio.com",
+  site: "https://kingludo.com",
   output: "static",
   adapter: cloudflare(),
 
